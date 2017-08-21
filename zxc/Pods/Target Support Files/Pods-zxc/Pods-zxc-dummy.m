5c41cba0127b8085d94b15d51968e164b8df2f4e
2464553424 <2464553424@qq.com>
2017-08-21 21:02:54 +0800

    iOS-first
    github

---
+++ zxc/Pods/Target Support Files/Pods-zxc/Pods-zxc-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_zxc : NSObject
+@end
+@implementation PodsDummy_Pods_zxc
+@end
---
+++ zxc/zxc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  zxc
+//
+//  Created by Fly on 2017/8/21.
+//  Copyright © 2017年 阿斯顿. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
